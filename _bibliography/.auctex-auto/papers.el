7d33d4808a058d64593847c15f5f163a9d072996
Vision314 <francescom.marrocco@gmail.com>
2026-05-15 13:53:34 -0400

    citations fix

--- _bibliography/.auctex-auto/papers.el
+++ _bibliography/.auctex-auto/papers.el
@@ -4,15 +4,6 @@
  "papers"
  (lambda ()
    (LaTeX-add-bibitems
-    "einstein1920relativity"
-    "einstein1956investigations"
-    "einstein1950meaning"
-    "PhysRev.47.777"
-    "einstein1905molekularkinetischen"
-    "einstein1905movement"
-    "einstein1905electrodynamics"
-    "einstein1905photoelectriceffect"
-    "przibram1967letters"
     "10658797"))
  '(or :bibtex :latex))
 
