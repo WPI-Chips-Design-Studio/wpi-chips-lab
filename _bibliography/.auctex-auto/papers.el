8dfaf1562e77dcfc39d112041cd5ff357cb0382e
Vision314 <francescom.marrocco@gmail.com>
2026-05-15 13:21:14 -0400

    added test publication

---
+++ _bibliography/.auctex-auto/papers.el
@@ -0,0 +1,18 @@
+;; -*- lexical-binding: t; -*-
+
+(TeX-add-style-hook
+ "papers"
+ (lambda ()
+   (LaTeX-add-bibitems
+    "einstein1920relativity"
+    "einstein1956investigations"
+    "einstein1950meaning"
+    "PhysRev.47.777"
+    "einstein1905molekularkinetischen"
+    "einstein1905movement"
+    "einstein1905electrodynamics"
+    "einstein1905photoelectriceffect"
+    "przibram1967letters"
+    "10658797"))
+ '(or :bibtex :latex))
+
